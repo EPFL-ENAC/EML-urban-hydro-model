9f954f4fdbea4e1b9b722a81ffb76d54c605e1bd
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-03 12:42:23 +0100

    test: add running example script

--- tests/test_dummy.py
+++
@@ -1,4 +0,0 @@
-def test_dummy():
-    import eml_urban_hydro_model
-
-    assert eml_urban_hydro_model is not None
---
+++ tests/test_example.py
@@ -0,0 +1,27 @@
+import os
+
+import pandas as pd
+
+import eml_urban_hydro_model as uhm
+
+
+def test_example():
+    """Test running the example."""
+
+    filepath = os.path.join(os.path.dirname(__file__), "..", "examples", "pluvio.pkl")
+    df = pd.read_pickle(filepath)
+
+    area_params = uhm.area_params["swisstech01"]
+    soil_params = uhm.soil_params["loamy sand"]
+
+    df_out = uhm.model_st(
+        df,
+        uhm.ModelParameters(
+            k=90,
+            lag=0,
+            soil_params=soil_params,
+            area_params=area_params,
+        ),
+    )
+
+    assert isinstance(df_out, pd.DataFrame)
